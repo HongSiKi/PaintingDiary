573d314cbeabc4f28d3a7217569346b80c633b12
Tory <88531407+1two13@users.noreply.github.com>
2022-12-21 17:15:22 +0900

    chore: eslint 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
-root.render(<h1>Hello React!</h1>);
+const container = document.getElementById('root');
+if (container !== null) {
+  const root = createRoot(container);
+  root.render(<h1>Hello React!</h1>);
+} else {
+  alert('root DOM이 없습니다.');
+}
