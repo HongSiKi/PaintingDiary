7e7b86de20b6cd3041e30dfe7f4820186909a634
Tory <88531407+1two13@users.noreply.github.com>
2022-12-26 18:45:25 +0900

    feat: tailwind.config.js 파일에 사용할 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      black: 333333,
+      deepGray: 555555,
+      lightGray: '#eeeeee',
+    },
   },
   plugins: [],
 };
