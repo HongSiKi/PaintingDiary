d744e724b61b7945cb6c3ea1cac746f1d9e312d6
Tory <88531407+1two13@users.noreply.github.com>
2022-12-27 11:26:06 +0900

    feat: 로그인 클릭했을 때 팝업창 보여주기

--- src/components/landing/UnLoggedMenu.tsx
+++ src/components/landing/UnLoggedMenu.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Logo from '../../../public/images/Logo.jpeg';
+import Modal from './Modal';
 
 function UnLoggedMenu() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -10,11 +11,12 @@ function UnLoggedMenu() {
   };
 
   return (
-    <div className="flex p-3 border-b-2 border-lightGray text-black">
+    <div className="flex p-3 border-b-2 border-deepGray text-deepBlack">
       <div className="aspect-square w-[40px] min-w-[40px] ml-3 mr-5">
         <img src={Logo}></img>
       </div>
       <button onClick={showModal}>로그인</button>
+      {modalOpen && <Modal setModalOpen={setModalOpen} />}
     </div>
   );
 }
