4d47ce81c0d90b67ebf405714b5ff398a9696a19
Tory <88531407+1two13@users.noreply.github.com>
2023-01-02 16:17:57 +0900

    refactor: 사용하지 않는 속성 제거

--- src/components/landing/Portfolio.tsx
+++ src/components/landing/Portfolio.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 function Portfolio() {
   return (
     <div className="h-[100%]">
-      <iframe
-        src="https://lr.doesbook.kr/"
-        title="portfolio"
-        allow="autoplay"
-        className="w-[100%] h-[100%]"
-      >
+      <iframe src="https://lr.doesbook.kr/" title="portfolio" className="w-[100%] h-[100%]">
         포트폴리오가 없습니다.
       </iframe>
     </div>
