14149b1f8ec82bb3ab19657ba01e129629b6edf1
Tory <88531407+1two13@users.noreply.github.com>
2023-01-02 22:10:01 +0900

    feat: 3D 이미지를 가져오는 컴포넌트 추가

---
+++ src/components/landing/Shiba.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useGLTF } from '@react-three/drei';
+
+function Shiba() {
+  const {
+    nodes: { Sketchfab_model: shibaObject },
+  } = useGLTF('/resources/shiba.gltf');
+
+  return <primitive object={shibaObject} />;
+}
+
+export default Shiba;
