900b381e5eff4263d87ca0db89cbb5ac557800f8
Tory <88531407+1two13@users.noreply.github.com>
2023-01-02 22:00:38 +0900

    fix: eslint 오류 해결
    Unknown property 'object' found  react/no-unknown-property

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import', 'prettier'],
   rules: {
+    'react/no-unknown-property': ['error', { ignore: ['object'] }],
     'prettier/prettier': [
       'error',
       {
