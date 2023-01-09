a674ee53e31d5cbd04c7a21f175e23bac657f56d
Tory <88531407+1two13@users.noreply.github.com>
2023-01-09 23:34:13 +0900

    refactor: 주석 제거

--- src/components/makeCharacter/Skill.tsx
+++ src/components/makeCharacter/Skill.tsx
@@ -27,20 +27,6 @@ function Skill({ countList }: any) {
         </div>
       ))}
     </>
-
-    // {countList.map((key: number) => (
-    //   <div key={key} className="max-h-[72%] pl-[1%] ">
-    //     <div className="flex mb-[1%]">
-    //       <input placeholder="기술" className="w-[40%] bg-deepGray text-center mr-[2%]" />
-    //       {/* <div className="border-2 w-[100%]"> */}
-    //       {/* <input onChange={onChange} placeholder="숫자만 작성해주세요.(ex.80)" /> */}
-    //       <div className="w-[100%] bg-deepGray">
-    //         <div className="h-[100%] w-[80%] bg-yellow" />
-    //       </div>
-    //     </div>
-    //     <input placeholder="자세한 설명" className="w-[100%] mb-[2%] pl-[1%] bg-deepGray" />
-    //   </div>
-    // ))}
   );
 }
 
