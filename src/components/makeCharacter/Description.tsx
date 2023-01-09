e63e29cfa6b3259dcb262ef6f00603c79465d8e4
Tory <88531407+1two13@users.noreply.github.com>
2023-01-09 23:17:30 +0900

    feat: 스킬추가 컴포넌트 추가

--- src/components/makeCharacter/Description.tsx
+++ src/components/makeCharacter/Description.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import Button from './Button';
+import Nickname from './Nickname';
+import Skills from './Skills';
 
 function Description() {
   return (
     <div className="flex flex-col w-[70%] h-[70%] pl-[3%]">
-      <div className="flex h-[8%] mt-[2%] justify-between">
+      <Nickname />
+      <input
+        placeholder="자기소개 링크(optional)"
+        className="h-[8%] mt-[1%] pl-[1%] border-2 border-deepGray"
+      />
+      <Skills />
+      <div className="flex h-[8%] mt-[5%] justify-between">
         <Button text="생성완료" />
         <Button text="취소" />
       </div>
