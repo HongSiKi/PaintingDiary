d0759958cd2a0b260596cf3a32ebefd6d86217d2
Tory <88531407+1two13@users.noreply.github.com>
2023-01-10 17:24:59 +0900

    feat: props 추가

--- src/components/makeCharacter/Button.tsx
+++ src/components/makeCharacter/Button.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 interface IButton {
   text: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
-function Button({ text, onClick }: IButton) {
+function Button({ text, onClick, disabled }: IButton) {
   return (
     <button
       type="button"
       onClick={onClick}
+      disabled={disabled}
       className="w-[48%] bg-deepGray text-center cursor-pointer"
     >
       {text}
