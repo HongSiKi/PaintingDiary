3a28435882aaea782b9d2124cdb7decb4f5cea56
Tory <88531407+1two13@users.noreply.github.com>
2023-01-17 09:48:47 +0900

    feat: 에셋 관리 패널 생성(배경, 바닥, 소품)

--- src/components/decorateMyRoom/Assets.tsx
+++ src/components/decorateMyRoom/Assets.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Assets() {
-  return <div className="w-[25%] bg-deepGray">Asset관리영역</div>;
+  return (
+    <div className="flex flex-col justify-evenly w-[25%] p-[1.5%] bg-deepGray">
+      <div className="h-[25%] p-[2%] bg-[white]">배경</div>
+      <div className="h-[25%] p-[2%] bg-[white]">바닥</div>
+      <div className="h-[25%] p-[2%] bg-[white]">소품</div>
+    </div>
+  );
 }
 
 export default Assets;
