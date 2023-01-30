1edbf2d3dee82248a728a99cdacf834229a1dee8
Tory <88531407+1two13@users.noreply.github.com>
2023-01-30 11:55:49 +0900

    refactor: 사용하지 않는 import문 제거

--- src/components/makeCharacter/Nickname.tsx
+++ src/components/makeCharacter/Nickname.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hook';
 
 import nicknameSlice from '../../redux/slices/nicknameSlice';
