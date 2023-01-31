215c4f25501ecfd214e979106f36cd45194c3688
Tory <88531407+1two13@users.noreply.github.com>
2023-01-31 15:55:20 +0900

    feat: 스킬 추가 버튼을 클릭했을 때 skillList에 skill 추가, UI 변경

--- src/components/makeCharacter/Skills.tsx
+++ src/components/makeCharacter/Skills.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
+import { useAppSelector, useAppDispatch } from '../../redux/hook';
+
+import makeCharacterSlice from '../../redux/slices/makeCharacterSlice';
 
 import Skill from './Skill';
 
 function Skills() {
-  const [countList, setCountList] = useState([1]);
+  const dispatch = useAppDispatch();
+  const [countList, setCountList] = useState<number[]>([]);
+  const [isClick, setIsClick] = useState(false);
+  const skill = useAppSelector((state) => state.makeCharacter.skill);
 
   const addSkill = () => {
     const countArr = [...countList];
@@ -11,19 +17,28 @@ function Skills() {
     counter += 1;
     countArr.push(counter);
     setCountList(countArr);
+    setIsClick(true);
+    dispatch(makeCharacterSlice.actions.skillList(skill));
   };
 
   return (
     <>
-      <div className="flex justify-between mt-[1%] mx-[1%]">
-        <div>스킬(자신의 어필 포인트를 적어주세요)</div>
-        <div>{countList.length} / 5</div>
-      </div>
-      <Skill countList={countList} />
+      {isClick ? (
+        <>
+          <div className="flex justify-between mt-[1%] mx-[1%]">
+            <div>스킬(자신의 어필 포인트를 적어주세요)</div>
+            <div>{countList.length} / 5</div>
+          </div>
+          <Skill countList={countList} />
+        </>
+      ) : (
+        ''
+      )}
+
       <button
         onClick={addSkill}
         type="button"
-        className="h-[8%] pl-[1%] rounded-[23px] bg-yellow"
+        className="h-[8%] pl-[1%] mt-[1%] rounded-[23px] bg-yellow"
         disabled={countList.length >= 5}
       >
         스킬 추가
