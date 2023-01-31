fbf97e8a8e9d8f4496e0183b2fcbd346ba564092
Tory <88531407+1two13@users.noreply.github.com>
2023-01-31 15:50:36 +0900

    refactor: 작성되는 스킬, 클릭, 설명이 redux로 관리될 수 있도록 수정

--- src/components/makeCharacter/Skill.tsx
+++ src/components/makeCharacter/Skill.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { useAppDispatch } from '../../redux/hook';
+
+import makeCharacterSlice from '../../redux/slices/makeCharacterSlice';
 
 function Skill({ countList }: { countList: number[] }) {
+  const dispatch = useAppDispatch();
   const [progress, setProgress] = useState<Record<string, number>>({});
 
   useEffect(() => {
@@ -13,8 +17,26 @@ function Skill({ countList }: { countList: number[] }) {
     });
   }, [countList]);
 
-  const progressCount = (key: number) => {
-    setProgress({ ...progress, [key]: (progress[key] % 5) + 1 });
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    dispatch(makeCharacterSlice.actions.title({ title: e.target.value, index: index - 1 }));
+  };
+
+  const progressCount = (index: number) => {
+    setProgress({ ...progress, [index]: (progress[index] % 5) + 1 });
+
+    if (progress[index] === 5) {
+      dispatch(makeCharacterSlice.actions.progress({ progress: 1, index: index - 1 }));
+    } else {
+      dispatch(
+        makeCharacterSlice.actions.progress({ progress: progress[index] + 1, index: index - 1 }),
+      );
+    }
+  };
+
+  const onChangeDescription = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    dispatch(
+      makeCharacterSlice.actions.description({ description: e.target.value, index: index - 1 }),
+    );
   };
 
   type widthType = Record<number, string>;
@@ -31,7 +53,11 @@ function Skill({ countList }: { countList: number[] }) {
       {countList.map((key: number) => (
         <div key={key} className="max-h-[72%] pl-[1%]">
           <div className="flex mb-[1%]">
-            <input placeholder="기술" className="w-[40%] mr-[2%] text-center bg-deepGray" />
+            <input
+              onChange={(e) => onChangeTitle(e, key)}
+              placeholder="스킬"
+              className="w-[40%] mr-[2%] text-center bg-deepGray"
+            />
 
             <div
               role="none"
@@ -47,7 +73,11 @@ function Skill({ countList }: { countList: number[] }) {
             </div>
           </div>
 
-          <input placeholder="자세한 설명" className="w-[100%] mb-[2%] pl-[1%] bg-deepGray" />
+          <input
+            onChange={(e) => onChangeDescription(e, key)}
+            placeholder="자세한 설명"
+            className="w-[100%] mb-[2%] pl-[1%] bg-deepGray"
+          />
         </div>
       ))}
     </>
