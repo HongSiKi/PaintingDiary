ea3a8c154cc4e532c27a9945c29248b287172a67
Tory <88531407+1two13@users.noreply.github.com>
2023-02-01 12:41:32 +0900

    feat: 캐릭터 생성하는 서버 연동

--- src/components/makeCharacter/Description.tsx
+++ src/components/makeCharacter/Description.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../../redux/hook';
+import { useAppSelector, useAppDispatch } from '../../redux/hook';
+
+import makeCharacterSlice from '../../redux/slices/makeCharacterSlice';
 
 import Button from './Button';
 import Nickname from './Nickname';
@@ -8,16 +10,46 @@ import Skills from './Skills';
 
 function Description() {
   const navigate = useNavigate();
-  const moveToMain = () => {
-    navigate('/');
-  };
+  const dispatch = useAppDispatch();
   const message = useAppSelector((state) => state.nickname.message);
   const isDuplicate = useAppSelector((state) => state.nickname.isDuplicate);
+  const nickname = useAppSelector((state) => state.nickname.nickname);
+  const link = useAppSelector((state) => state.makeCharacter.link);
+  const skillList = useAppSelector((state) => state.makeCharacter.skillList);
+
+  const onChangeLink = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(makeCharacterSlice.actions.updateLink(e.target.value));
+  };
+
+  const moveToMain = () => {
+    const url = '/api/characters';
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        nickname,
+        link,
+        skill_list: skillList,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        console.log(result);
+        return result;
+      });
+
+    navigate('/');
+  };
 
   return (
     <div className="flex flex-col w-[70%] h-[70%] pl-[3%]">
       <Nickname />
       <input
+        onChange={onChangeLink}
         placeholder="자기소개 링크(optional)"
         className="h-[8%] mt-[1%] pl-[1%] border-2 border-deepGray"
       />
