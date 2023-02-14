d70041722dbbbf3acf96b2f3b400aff811bc7c3f
Tory <88531407+1two13@users.noreply.github.com>
2023-02-14 10:42:57 +0900

    feat: props로 받아온 index에 따라 캐릭터 보여주기

--- src/components/makeCharacter/Character.tsx
+++ src/components/makeCharacter/Character.tsx
@@ -2,23 +2,39 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import CharacterProfile from '../landing/CharacterProfile';
-import Shiba from '../landing/Shiba';
+import Dog from '../landing/Shiba';
+import Cat from './Cat';
+import Rabbit from './Rabbit';
 
-function Character() {
+function Character({ index }: { index: number }) {
   const navigate = useNavigate();
   const onClick = () => {
     navigate('/decorateCharacter');
   };
 
   return (
-    <div className="w-[30%] h-[70%]">
-      <div className="h-[90%] bg-deepGray">
-        <CharacterProfile model={<Shiba />} />
+    <div className="h-[100%] w-[30%]">
+      <div className="h-[87%] bg-lightGray">
+        {index === 0 ? (
+          <CharacterProfile model={<Dog />} camera={{ position: [0.1, 0.1, 2] }} intensity={1.5} />
+        ) : (
+          ''
+        )}
+        {index === 1 ? (
+          <CharacterProfile model={<Cat />} camera={{ position: [1, 150, 700] }} intensity={1.3} />
+        ) : (
+          ''
+        )}
+        {index === 2 ? (
+          <CharacterProfile model={<Rabbit />} camera={{ position: [1, 0.1, 10] }} />
+        ) : (
+          ''
+        )}
       </div>
       <button
         onClick={onClick}
         type="button"
-        className="w-[100%] mt-[10%] bg-deepGray text-center cursor-pointer"
+        className="w-[100%] h-[8%] mt-[10%] bg-deepGray text-center cursor-pointer"
       >
         커스터마이징
       </button>
