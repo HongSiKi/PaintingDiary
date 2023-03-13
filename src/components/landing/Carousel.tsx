eff1926bd5910c4c40874c695bd85902ec1194bc
Tory <88531407+1two13@users.noreply.github.com>
2023-03-13 11:39:45 +0900

    refactor: 중복되는 코드 제거

--- src/components/landing/Carousel.tsx
+++ src/components/landing/Carousel.tsx
@@ -7,30 +7,23 @@ interface ICarousel {
 function Carousel({ elements }: ICarousel) {
   const ref = useRef<HTMLDivElement>(null);
 
-  const prevButton = () => {
+  const onClickHandler = (offset: number) => {
     const div = ref.current;
     div?.scrollTo({
-      left: div.scrollLeft - 200,
-      behavior: 'smooth',
-    });
-  };
-  const nextButton = () => {
-    const div = ref.current;
-    div?.scrollTo({
-      left: div.scrollLeft + 200,
+      left: div.scrollLeft + offset,
       behavior: 'smooth',
     });
   };
 
   return (
     <div className="flex flex-row h-[30%] mx-[1%]">
-      <button type="button" onClick={prevButton} className="text-[200%] w-[5%]">
+      <button type="button" onClick={() => onClickHandler(-200)} className="text-[200%] w-[5%]">
         &lt;
       </button>
       <div ref={ref} className="flex flex-row overflow-scroll">
         {elements}
       </div>
-      <button type="button" onClick={nextButton} className="text-[200%] w-[5%]">
+      <button type="button" onClick={() => onClickHandler(200)} className="text-[200%] w-[5%]">
         &gt;
       </button>
     </div>
