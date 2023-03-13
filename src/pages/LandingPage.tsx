5ab4ef912195de9a8d2cd23155d8b7b72dd43f36
Tory <88531407+1two13@users.noreply.github.com>
2023-03-13 15:42:29 +0900

    feat: 카드 구현

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,25 +5,57 @@ import CharacterDescription from '../components/landing/CharacterDescription';
 
 function LandingPage() {
   const imgSrcList = [
-    'https://www.10wallpaper.com/wallpaper/1366x768/1411/puppy_retriever_rest-Animal_Photo_Wallpaper_1366x768.jpg',
-    'https://www.10wallpaper.com/wallpaper/1920x1440/1409/Cute_Little_Puppy-photography_HD_wallpaper_1920x1440.jpg',
-    'https://post-phinf.pstatic.net/MjAxNzA4MjVfMTY3/MDAxNTAzNjM0Njg4OTg4.bn6J8-JuqtMRhvPYSt8psWGHFxDTdd5oP6WrS_IL-C4g.52DnCYHKYntko0agq04Fq4cJyVaJsg4k3QrQDrjzbK4g.JPEG/%EA%B0%95%EC%95%84%EC%A7%80026.jpg?type=w1200',
-    'https://img.danawa.com/images/descFiles/5/279/4278439_1591530897943.jpeg',
-    'https://i.pinimg.com/474x/19/97/75/199775b7651168880acdbf52f2c7c72a.jpg',
-    'https://file.thisisgame.com/upload/tboard/user/2016/05/29/20160529004354_7091.jpeg',
+    {
+      id: 1,
+      src: 'https://www.10wallpaper.com/wallpaper/1366x768/1411/puppy_retriever_rest-Animal_Photo_Wallpaper_1366x768.jpg',
+      text: '리트리버',
+    },
+    {
+      id: 2,
+      src: 'https://www.10wallpaper.com/wallpaper/1920x1440/1409/Cute_Little_Puppy-photography_HD_wallpaper_1920x1440.jpg',
+      text: '웰시코기',
+    },
+    {
+      id: 3,
+      src: 'https://post-phinf.pstatic.net/MjAxNzA4MjVfMTY3/MDAxNTAzNjM0Njg4OTg4.bn6J8-JuqtMRhvPYSt8psWGHFxDTdd5oP6WrS_IL-C4g.52DnCYHKYntko0agq04Fq4cJyVaJsg4k3QrQDrjzbK4g.JPEG/%EA%B0%95%EC%95%84%EC%A7%80026.jpg?type=w1200',
+    },
+    {
+      id: 4,
+      src: 'https://img.danawa.com/images/descFiles/5/279/4278439_1591530897943.jpeg',
+      text: '시고르자브종',
+    },
+    {
+      id: 5,
+      src: 'https://i.pinimg.com/474x/19/97/75/199775b7651168880acdbf52f2c7c72a.jpg',
+    },
+    {
+      id: 6,
+      src: 'https://file.thisisgame.com/upload/tboard/user/2016/05/29/20160529004354_7091.jpeg',
+      text: '고양이와 강아지',
+    },
   ];
 
   return (
     <div>
       <Carousel
         elements={imgSrcList.map((el, index) => (
-          <img
+          <div
             key={index}
-            alt={el}
-            src={el}
-            // 기본값 50, sm, md, lg에 따라 사진 비율 변경
-            className="aspect-square px-[1%] w-[50%] sm:w-[38%] md:w-[30%] lg:w-[18%]"
-          />
+            className="flex flex-col aspect-square m-[1%] w-[50%] sm:w-[38%] md:w-[30%] lg:w-[18%] border-[1px] border-deepGray shadow"
+          >
+            {el.text ? (
+              <img
+                alt={el.id.toString()}
+                src={el.src}
+                className="h-[90%] sm:h-[85%] md:h-[83%] lg:h-[90%]"
+              />
+            ) : (
+              <img alt={el.id.toString()} src={el.src} className="h-[100%]" />
+            )}
+            <div className="flex min-h-[10%] justify-center items-center font-semibold">
+              {el.text}
+            </div>
+          </div>
         ))}
       />
       <CharacterDescription />
